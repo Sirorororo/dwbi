eeb5128a12a6fe4fc543eb10275a9793f1c4a37c
Sirorororo <acharyanuwakot123@gmail.com>
2024-12-28 21:25:12 +0545

    Create Class Definitions for connecting to database

--- library/Database.py
+++ library/Database.py
@@ -1,17 +1,36 @@
 import mysql.connector
+from Variables import Variables
+import pandas as pd
 
 class Database:
-    def __init__(self):
-        pass
-
-    def connect(self):
-        pass
-
-    def execute_query(self):
-        pass
+    def __init__(self,database):
+        
+        try:
+            self.conn = mysql.connector.connect(
+                host = Variables("host").get_variable(),
+                port =  Variables("port").get_variable(),
+                user =  Variables("user").get_variable(),
+                password =  Variables("password").get_variable(),
+                database = database
+            )
+            if self.conn.is_connected():
+                print("Successfully connected to MySQL!")
+        
+        except mysql.connector.Error as e:
+            print(f"Error connecting to MySQL: {e}")
+        
+    def execute_query(self,query):
+        df = pd.read_sql(query,self.conn)
+        return df
 
     def disconnect(self):
-        pass
+        self.conn.close()
 
     def fetch(self):
         pass
+
+
+db = Database("bct2077")
+df = db.execute_query("SELECT * FROM teacher")
+df.to_csv("output.csv")
+db.disconnect()
--- library/Variables.py
+++ library/Variables.py
@@ -2,18 +2,20 @@ import json
 
 class Variables:
     def __init__(self,variable_name):
-        self.path = "C:/Users/achar/DWBI/config/config.cfg"
+        self.path = "D:\Study\Eighth Sem\DWBI\config\config.cfg"
         self.name = variable_name
 
     def get_variable(self):
+
         with open(self.path,"r") as file:
-
             file_content = json.loads(file.read())
-            print(file_content)
+            # print(file_content)
             return file_content[self.name]
+        
+# var = Variables("database")
+# database_name = Variables("database").get_variable()
 
 
-var = Variables("DATABASE_NAME")
+# database_name = Variables("database").get_variable()
 
-
-print(var.get_variable())
+# print(database_name)
